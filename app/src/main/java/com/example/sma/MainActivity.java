d733becae825959ca306231829fafc2070a17a58
Georgiana Sentaliu <gsentaliu@gmail.com>
2020-01-03 13:18:07 +0200

    first commit

---
+++ app/src/main/java/com/example/sma/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sma.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button uploadButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
